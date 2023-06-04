f18b2238f1b8ffb40c5603d599902c0151cae528
geihansyahlsdnv <114799997+geihansyahlsdnv@users.noreply.github.com>
2023-06-04 22:14:41 +0700

    Add files via upload

---
+++ HasilPertandingan1/src/main/java/com/gehan/hasilpertandingan1/HasilPertandingan1.java
@@ -0,0 +1,14 @@
+package com.gehan.hasilpertandingan1;
+
+
+
+public class HasilPertandingan1 {
+    
+    public static void main(String[] args) {
+        StartScreen form = new StartScreen();
+        form.show(true);
+        
+        
+    }
+}
+
